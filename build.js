aafa6abcbf74716685d905c487960234ee8aed0e
Dave Marr <davemarr@gmail.com>
2015-11-16 17:08:05 -0800

    Add CNAME

--- build.js
+++ build.js
@@ -1,11 +1,16 @@
 var Metalsmith = require('metalsmith');
 var layouts = require('metalsmith-layouts');
-var markdown = require('metalsmith-markdown');
+var markdown = require('metalsmith-markdownit');
 var sass = require('metalsmith-sass');
+var footnote = require('markdown-it-footnote');
 
+var md = markdown('default', {
+  footnote: true
+});
+md.parser.use(footnote);
 Metalsmith(__dirname)
-  .use(layouts('handlebars'))
-  .use(markdown())
+  //.use(layouts('handlebars'))
+  .use(md)
   .use(sass({ outputDir: 'css' }))
   .build(function(err) {
     if (err) {
